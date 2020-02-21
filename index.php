0a9a62959a9469d61862ca5f7f8e886ae4a9849c
qreboli <qreboli@wanadev.fr>
2020-02-21 14:23:23 +0100

    add volume db and baptou explain archi <3

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <h1>WANAFOOOT MAGGLE</h1>
 <?php
+use App\Route\Security;
 
+    switch ($_SERVER["REQUEST_URI"]) {
+        case "/login":
+            Security::login();
+            break;
+        default:
+            echo "Not found";
+    }
 ?>
