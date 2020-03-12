360e7c6dd83b9c2458befcf588c835fe3494de22
qreboli <qreboli@wanadev.fr>
2020-03-12 18:29:29 +0100

    create table with pdo

--- db/connection.php
+++ db/connection.php
@@ -1,11 +1,36 @@
 <?php
+
+$serverName = "db-foot";
+$dbName = "wanafoot";
+$port = 3306;
+$userName = "wanadev";
+$password = "secret";
+
 try
 {
-    $bdd = new PDO('mysql:host=db-foot;port=3306;dbname=wanafoot;charset=utf8', 'wanadev', 'secret');
+    $connection = new PDO("mysql:host=$serverName;port=$port;dbname=$dbName;charset=utf8", $userName, $password);
+    // set the PDO error mode to exception
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+    // sql to create table
+    $sql = "CREATE TABLE user (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    password VARCHAR(255),
+    reg_date TIMESTAMP
+    )";
+    // use exec() because no results are returned
+    $connection->exec($sql);
+    echo "Table users created successfully";
+
 }
+
 catch (Exception $e)
 {
     die('Erreur : ' . $e->getMessage());
 }
-echo 'bdd ok'
+echo 'bdd ok';
+
 ?>
